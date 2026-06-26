c73b0b88a58a9c4387b3cda73c7290102baf23a1
Rahmat Husain <raajhusain5@gmail.com>
2026-06-26 09:32:27 -0700

    Add application configuration

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import json
 import os
 import getpass
 
+CANDIDATE_FILE = "candidates.json"
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin123"
+
 print("===================================")
 print(" UNIVERSITY ELECTION VOTING SYSTEM ")
 print("===================================")
