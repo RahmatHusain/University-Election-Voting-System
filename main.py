64bef0056c9ee77f39663c29d38edc6d8cc241fe
Rahmat Husain <raajhusain5@gmail.com>
2026-06-26 09:36:20 -0700

    Implement candidate data loading

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+print("===================================")
+print(" UNIVERSITY ELECTION VOTING SYSTEM ")
+print("===================================")
+
 import json
 import os
 import getpass
@@ -7,6 +11,16 @@ CANDIDATE_FILE = "candidates.json"
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin123"
 
-print("===================================")
-print(" UNIVERSITY ELECTION VOTING SYSTEM ")
-print("===================================")
+def load_candidates():
+
+    if os.path.exists(CANDIDATE_FILE):
+
+        try:
+
+            with open(CANDIDATE_FILE, "r") as file:
+                return json.load(file)
+
+        except json.JSONDecodeError:
+            return []
+
+    return []
