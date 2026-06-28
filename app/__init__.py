d9ac2d690c123168dd0e7223d8ce9d9cc572d432
Rahmat Husain <raajhusain5@gmail.com>
2026-06-28 00:45:11 -0700

    Create reusable base template with Bootstrap layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
 db = SQLAlchemy()
-login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -11,7 +9,6 @@ def create_app():
     app.config.from_object("config.Config")
 
     db.init_app(app)
-    login_manager.init_app(app)
 
     from app.routes import main
     app.register_blueprint(main)
