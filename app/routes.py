835e8f720b0639b609680017b688035cb91e8297
Rahmat Husain <raajhusain5@gmail.com>
2026-06-28 01:12:21 -0700

    Add custom 404 and 500 error pages

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,13 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+
+@main.app_errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(error):
+    return render_template("errors/500.html"), 500
